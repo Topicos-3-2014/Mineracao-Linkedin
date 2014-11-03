f88390961614f36c785bfa3afda5d5c5892204aa
JorgePereiraUFRN <jorgepereirasb@gmail.com>
2014-11-03 10:56:04 -0300

    salvar contatos em arquivo

--- contatos.py
+++ contatos.py
@@ -17,6 +17,12 @@ app = server.quick_api(CONSUMER_KEY, CONSUMER_SECRET)
 profile = app.get_profile()
 connections = app.get_connections()
 
+connections_data = 'linkedin_connections_'+profile['firstName']+'.json'
+
+f = open(connections_data, 'w')
+f.write(json.dumps(connections, indent=1))
+f.close()
+
 G = nx.Graph(name="My Contacts linkedIn")
 
 G.add_node(profile['firstName'])
