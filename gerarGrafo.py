03f0f71151bc22afd1a6b4b44ec1e85f6dce2ca4
JorgePereiraUFRN <jorgepereirasb@gmail.com>
2014-11-18 11:23:20 -0300

    adicao das informacoes de nome e cargo aos nos do grafo

--- gerarGrafo.py
+++ gerarGrafo.py
@@ -8,23 +8,30 @@ import matplotlib.pyplot as plt
 def getLista(G):
 	nos = []
 	for x in range(0, G.number_of_nodes()):
+		#print G.node[x],"\n\n"
 		nos.append(G.node)
 		
 	return nos
 
 
-
-connections = json.loads(open('profiles').read())
-
-
 def gerargrafo(arquivo, contato):
 	G = nx.Graph(name="contato")
-	G.add_node(contato)
+
+	G.add_node(contato,nome = contato)
 	connections = json.loads(open(arquivo).read())
 
 	for c in connections['values']:
-		G.add_node(c['firstName'])
-		G.add_edge(c['firstName'], contato)
+		
+		dic = {}.fromkeys(['nome','cargo', 'nome'])
+		dic['nome'] =  c['firstName']
+		dic['cargo'] = c['headline']
+		dic['nome'] = c['firstName']
+		
+		#print dic,"\n"
+
+		G.add_node(c['firstName'] , dic)
+		G.add_edge( c['firstName'] , contato)
+
 	
 	return G
 
@@ -32,11 +39,16 @@ def gerargrafo(arquivo, contato):
 
 Grafo = nx.Graph(name="contatos")
 
+connections = json.loads(open('profiles').read())
+
 for c in connections:
+
+	print c
 
 	connections_data = 'linkedin_connections_'+c+'.json'
 	
 	G = gerargrafo(connections_data, c)
+
 
 	Grafo = nx.compose(Grafo, G)
 
@@ -44,14 +56,18 @@ for c in connections:
 	print('\n\n'+ nx.info(Grafo))
 
 
-lista = getLista(Grafo)
+#lista = getLista(Grafo)
 
 
-for s in lista:
-	print s
+for x in Grafo.nodes(data=True):
+	print x, "\n"
+
 
 nx.draw(Grafo)
+
 plt.show()
+
+
 	
 	
 
